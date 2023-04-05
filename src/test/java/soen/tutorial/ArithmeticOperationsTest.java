48cc54cdcd196507a53b233e0cb205ed3005960f
lyker <79274982+lyeskara@users.noreply.github.com>
2023-04-04 23:37:07 -0400

    updates to code

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -8,25 +8,25 @@ import static org.junit.Assert.fail;
 public class ArithmeticOperationsTest {
 
 
-	@Test
-	public void testAdd()
-	{
+	//@Test
+	//public void testAdd()
+	//{
 
 		ArithmeticOperations operations = new ArithmeticOperations();
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
-		assertEquals(expected, actual);
+		// assertEquals(expected, actual);
+                fail("Must fail");
+	//}
+    //@Test
+    //public void testSub()
+    //{
 
-	}
-    @Test
-    public void testSub()
-    {
-
-        ArithmeticOperations operations = new ArithmeticOperations();
-       Integer actual = operations.addOrSub(6, 2);
-       Integer expected = 4;
+        //ArithmeticOperations operations = new ArithmeticOperations();
+      // Integer actual = operations.addOrSub(6, 2);
+    //   Integer expected = 4;
   //      assertEquals(expected, actual);
     fail("must fail");
-    }
+  //  }
 //test
-}
+//}
