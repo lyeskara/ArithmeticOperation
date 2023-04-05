c1b3ea759bfff87a94e2657855c58603ccb4c92d
lyker <79274982+lyeskara@users.noreply.github.com>
2023-04-04 23:54:36 -0400

    Update ArithmeticOperationsTest.java

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -27,7 +27,7 @@ public class ArithmeticOperationsTest {
       // Integer actual = operations.addOrSub(6, 2);
     //   Integer expected = 4;
   //      assertEquals(expected, actual);
-    fail("must fail");
+  //  fail("must fail");
   //  }
 //test
 //}
